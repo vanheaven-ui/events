031c5d8e7b1c1342aca18642b4d19cd3da938c76
vanheaven <vanheaven6@gmail.com>
2020-09-20 11:17:42 +0300

    Add signout to sessions#welcome view template

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :users, only: %i[new create show]
   resources :sessions, only: %i[new create destroy welcome]
   get "/welcome", to: "sessions#welcome"
+  get "/sign_in", to: "sessions#new"
+  post "/sign_in", to: "sessions#create"
 end
