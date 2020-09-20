21267ef8769107c414760bc9f34bb24247e33ecc
vanheaven <vanheaven6@gmail.com>
2020-09-20 12:09:30 +0300

    Add events#new, create, show, and index actions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,23 @@
 class EventsController < ApplicationController
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.save
+
+    redirect_to root_path
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :description, :date, :location)
+  end
 end
