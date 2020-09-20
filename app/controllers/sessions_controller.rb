b76553aaf7365b5a70a80392d90c63cfff8be2d9
vanheaven <vanheaven6@gmail.com>
2020-09-20 10:25:40 +0300

    Add sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+
+  def new; end
+
+  def create
+    @user = User.find_by(name: params[:name])
+    session[:user_id] = @user.id
+
+    redirect_to root_url
+  end
+
+  def destroy
+    session.destroy
+
+    redirect_to root_url
+  end
+end
