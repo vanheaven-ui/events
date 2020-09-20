787787aa869b53a6898d8c858f3de4fa16f4fa53
vanheaven <vanheaven6@gmail.com>
2020-09-20 13:18:59 +0300

    Add scope for upcoming and previous events to Event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :event_attendings, foreign_key: "attended_event_id"
   has_many :event_attendees, through: "event_attendings", source: "event_attendee"
+
+  scope: :upcoming_events -> { where("date > ?", Date.today) }
+  scope: :previous_events -> { where("date < ?", Date.today)}
 end
