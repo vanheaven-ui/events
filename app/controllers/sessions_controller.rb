628a19ab2229cc0c2a3b593473382b824c3fad10
vanheaven <vanheaven6@gmail.com>
2020-09-20 22:57:41 +0300

    Add validation constraint for User name

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,15 +4,19 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(name: params[:name])
-    session[:user_id] = @user.id
+    if @user
+      session[:user_id] = @user.id
 
-    redirect_to root_url
+      redirect_to root_url, notice: "Successfully signed in, cheers!"
+    else
+      render 'new', alert: "Oops! cannot sign in, check and try again or sign up"
+    end
   end
 
   def destroy
     session.destroy
 
-    redirect_to root_url
+    redirect_to root_url, notice: "Successfully signed out!"
   end
 
   def welcome; end
