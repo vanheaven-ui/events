f12ba4b085bbabd15bbe340db00b45749bca0e56
vanheaven <vanheaven6@gmail.com>
2020-09-22 06:25:54 +0300

    Modify UI

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user
   helper_method :signed_in?
+  helper_method :new_event
 
   def current_user
     User.find_by(id: session[:user_id])
@@ -9,5 +10,13 @@ class ApplicationController < ActionController::Base
 
   def signed_in?
     current_user
+  end
+
+  def new_event
+    if signed_in?
+      redirect_to new_event_path
+    else
+      redirect_to root_path, alert: "You must login first"
+    end
   end
 end
