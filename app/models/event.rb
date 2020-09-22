3894e0a835ba7d483c1e94e644cacccf0c8b30b3
vanheaven <vanheaven6@gmail.com>
2020-09-22 22:50:03 +0300

    Modify UI

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,7 @@ class Event < ApplicationRecord
   has_many :event_attendings, foreign_key: 'attended_event_id'
   has_many :event_attendees, through: 'event_attendings', source: 'event_attendee'
 
-  scope :upcoming_events, -> { where('date > ?', Date.today).order('date DESC') }
+  scope :upcoming_events, -> { where('date >= ?', Date.today).order('date DESC') }
   scope :previous_events, -> { where('date < ?', Date.today).order('date DESC') }
 
   def past?
