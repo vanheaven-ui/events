10472460272bcc108191a388d81f9e5ac42768d3
vanheaven <vanheaven6@gmail.com>
2020-09-22 07:53:52 +0300

    Add index for creator_id in events table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_20_094929) do
+ActiveRecord::Schema.define(version: 2020_09_22_044030) do
 
   create_table "event_attendings", force: :cascade do |t|
     t.integer "event_attendee_id", null: false
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2020_09_20_094929) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "creator_id"
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
