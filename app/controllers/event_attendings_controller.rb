df039c7b9eeae14a4da30b834f7ed7ed508e55ea
vanheaven <vanheaven6@gmail.com>
2020-09-26 10:42:04 +0300

    Remove duplicate routes

--- app/controllers/event_attendings_controller.rb
+++ app/controllers/event_attendings_controller.rb
@@ -3,6 +3,6 @@ class EventAttendingsController < ApplicationController
     @event = Event.find(params[:event_id])
     @user = User.find(params[:user_id])
     @event.event_attendees << @user
-    redirect_to event_path(@event), notice: 'Attendee added' if @event.save
+    redirect_to event_path(@event), notice: 'Attendee list updated' if @event.save
   end
 end
