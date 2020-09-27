ad4c82b2f61c53405e0acf9a249e6b956f32e02a
vanheaven <vanheaven6@gmail.com>
2020-09-27 14:52:54 +0300

    Modify EventAttendings controller

--- app/controllers/event_attendings_controller.rb
+++ app/controllers/event_attendings_controller.rb
@@ -1,6 +1,6 @@
 class EventAttendingsController < ApplicationController
   def create
-    @event = Event.find_by(id: params[:id])
+    @event = Event.find_by(id: params[:user_id])
     if @event.event_attendees.include? current_user
       flash[:alert] = 'You are already an attendee'
     else
