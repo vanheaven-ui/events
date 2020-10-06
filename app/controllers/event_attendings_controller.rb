71079a7a6c34fd951feaf1ce47ef14a6c5211312
vanheaven <vanheaven6@gmail.com>
2020-10-06 15:41:52 +0300

    Modify attendance process

--- app/controllers/event_attendings_controller.rb
+++ app/controllers/event_attendings_controller.rb
@@ -1,12 +1,12 @@
 class EventAttendingsController < ApplicationController
   
   def create
-    @event = Event.find(params[:id])
+    @event = Event.find(params[:event_id])
     if @event.event_attendees.include? current_user
       redirect_to event_path(@event), alert: 'You are already attendee'
     else
       @event.event_attendees << current_user
       redirect_to event_path(@event), notice: 'Attendance confirmed'
     end
-  end
+  end 
 end
