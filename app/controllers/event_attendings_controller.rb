f2301ca6bfca72592a881d10bbc51c695e64d500
vanheaven <vanheaven6@gmail.com>
2020-10-06 15:49:13 +0300

    Resolve rubocop offenses

--- app/controllers/event_attendings_controller.rb
+++ app/controllers/event_attendings_controller.rb
@@ -1,5 +1,4 @@
 class EventAttendingsController < ApplicationController
-  
   def create
     @event = Event.find(params[:event_id])
     if @event.event_attendees.include? current_user
@@ -8,5 +7,5 @@ class EventAttendingsController < ApplicationController
       @event.event_attendees << current_user
       redirect_to event_path(@event), notice: 'Attendance confirmed'
     end
-  end 
+  end
 end
